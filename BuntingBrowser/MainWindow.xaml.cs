7b142256127ba4ad36905c43e19068d09fabf19e
Sam Bunting <sambunting2008@live.co.uk>
2016-06-29 14:05:18 +0100

    Transfered over to a WPF application

--- BuntingBrowser/MainWindow.xaml.cs
+++ BuntingBrowser/MainWindow.xaml.cs
@@ -20,9 +20,74 @@ namespace BuntingBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string homepage = "http://bing.com";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            webBrowser1.Navigate(homepage);
+        }
+      
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                navigate();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            navigate();
+        }
+
+        private void navigate()
+        {
+            goButton.IsEnabled = false;
+            urlBar.IsEnabled = false;
+
+            if (!urlBar.Text.StartsWith("http://")) {
+                urlBar.Text = "http://" + urlBar.Text;
+            }
+            else
+            {
+
+            }
+
+            webBrowser1.Navigate(urlBar.Text);
+        }
+
+        private void webBrowser1_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            goButton.IsEnabled = true;
+            urlBar.IsEnabled = true;
+            urlBar.Text = webBrowser1.Source.AbsoluteUri;
+
+            if (webBrowser1.Source.AbsoluteUri.StartsWith("https://"))
+            {
+                urlBar.Background = Brushes.LightGreen;
+            }
+            else
+            {
+                urlBar.Background = Brushes.White;
+            }
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser1.CanGoBack) 
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void forwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
         }
     }
 }
